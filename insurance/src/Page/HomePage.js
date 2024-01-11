5054ef7f3bc6f95c598c9df8a71e3a8ba97f32cb
lotusSunachi <lotus.sunachi@gmail.com>
2024-01-11 23:16:23 +0700

    fix navication

---
+++ insurance/src/Page/HomePage.js
@@ -0,0 +1,16 @@
+
+import '../App.css';
+
+
+function HomePage() {
+ 
+
+  return (
+
+    <div>
+        <iframe title="embedded-html-page" src="http://127.0.0.1:5500/pageHtml/HomePage.html" width="100%" height="500px" />
+    </div>
+  );
+}
+
+export default HomePage;
