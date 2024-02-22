d85747596fec23930da3b715a5d9ab7db557c645
ChungTV <chungtv.it.t1001@gmail.com>
2024-02-22 07:50:26 +0700

    add common

---
+++ Server/Common/IBaseMapper.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Common
+{
+    public interface IBaseMapper<TSource, TDestination>
+    {
+        TDestination MapModel(TSource source);
+        IEnumerable<TDestination> MapList(IEnumerable<TSource> source);
+    }
+}
