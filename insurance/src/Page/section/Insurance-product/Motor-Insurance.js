f653e85bc88af6d7f74fdb7fd150f5b124d66607
lotusSunachi <lotus.sunachi@gmail.com>
2024-02-26 08:37:03 +0700

    update

--- insurance/src/Page/section/Insurance-product/Motor-Insurance.js
+++ insurance/src/Page/section/Insurance-product/Motor-Insurance.js
@@ -1,6 +1,6 @@
-import Header from "../../Component/header";
-import Footer from "../../Component/footer";
-import BusinessLocation from "../../Component/Business-location";
+import Header from "../../../Component/header";
+import Footer from "../../../Component/footer";
+import BusinessLocation from "../../../Component/Business-location";
 import { useContext, useState, useEffect, use } from "react";
 import "./insurance-product.css";
 import {
@@ -13,14 +13,16 @@ import {
   TextField,
   Autocomplete,
   Link,
+  Snackbar,
+  Alert,
 } from "@mui/material";
 
-import { DataContext } from "../../Context/data-context";
+import { DataContext } from "../../../Context/data-context";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import "../../App.css";
+import "../../../App.css";
 import axios from "axios";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
@@ -34,6 +36,9 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import moment from "moment";
+import SupportRegistration from "../../../Component/SupportRegistrationForm";
+import { useNavigate } from "react-router-dom";
 
 const City = ["Tp.Hcm", "Hà Nội"];
 
@@ -41,40 +46,39 @@ function LifeInsurance() {
   const [insuranceType, setInsuranceType] = useState({});
   const [finterData, setFinterData] = useState([]);
   const [otherProduct, setOtherProduct] = useState([]);
-  const { insuranceTypes, productNews } = useContext(DataContext);
   const [openDialog, setOpenDialog] = useState(false);
+  const [dataProvince, setDataProvince] = useState([]);
+  const [alertCheck, setAlertCheck] = useState(false);
+    const navigate = useNavigate();
 
-  const handleClickOpenDialog = () => {
-    setOpenDialog(true);
+  const { provinces, getAllProvince,insuranceTypes, productNews } = useContext(DataContext);
+  const date = moment().utcOffset("+07:00").format("YYYY-MM-DDThh:mm:ss");
+
+
+  const handleClickBuyNow = () => {
+    navigate("/insurance-online/Buy-Insurance", { state: { insuranceType } });
   };
 
-  const handleClose = () => {
-    setOpenDialog(false);
+  const handleCloseAlert = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setAlertCheck(false);
   };
 
-  const formik = useFormik({
-    initialValues: {
-      fullName: "",
-      phoneNumber: "",
-      Email: "",
-      gender: null,
-      selectedCity: null,
-    },
-    validationSchema: Yup.object({
-      fullName: Yup.string().required("Field is required"),
-      phoneNumber: Yup.string().required("Phone Number is required"),
-      email: Yup.string().email("Invalid email format"),
-    }),
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-      console.log(values.fullName);
-      setOpenDialog(false);
-    },
-  });
+  useEffect(() => {
+    if (getAllProvince.results != undefined) {
+      const provinceIds = getAllProvince.results;
+      setDataProvince(provinceIds);
+    }
+  }, [getAllProvince]);
+
+  
 
   useEffect(() => {
     const filteredInsuranceTypes = insuranceTypes.filter(
-      (item) => item.typeId === 2
+      (item) => item.typeId === 3
     );
     if (filteredInsuranceTypes.length > 0) {
       setInsuranceType(filteredInsuranceTypes[0]);
@@ -129,7 +133,7 @@ function LifeInsurance() {
                 color: "#fff",
               }}
             >
-             {insuranceType.typeName}
+              {insuranceType.typeName}
             </Typography>
           </Stack>
         </Stack>
@@ -174,18 +178,20 @@ function LifeInsurance() {
                 direction={{ xs: "column", sm: "row" }}
                 spacing={{ xs: 1, sm: 2, md: 4 }}
               >
-                <Button
+                {/* <Button
                   size="small"
                   className="btn-hea-info-green"
                   variant="contained"
                   onClick={handleClickOpenDialog}
                 >
                   Further Advice
-                </Button>
+                </Button> */}
+
                 <Button
                   size="small"
                   className="btn-hea-info-white"
                   variant="contained"
+                  onClick={handleClickBuyNow}
                 >
                   Buy now
                 </Button>
@@ -224,7 +230,7 @@ function LifeInsurance() {
           >
             <Stack sx={{ flex: 1 }}>
               <img
-                src={require("../../Img/dieukienthamgia.jpg")}
+                src={require("../../../Img/dieukienthamgia.jpg")}
                 alt="Insurance House"
                 style={{
                   width: "100%",
@@ -348,146 +354,7 @@ function LifeInsurance() {
           </Stack>
         </Box>
         {/* Tư vấn */}
-        <Stack
-          direction={{ xs: "column", sm: "row" }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-          sx={{
-            margin: {
-              xs: "60px 10px",
-              sm: "80px 420px",
-            },
-            height: "100%",
-
-            display: "flex", // Sử dụng flex để chia cấp chiều rộng và chiều cao
-          }}
-        >
-          <Stack sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
-            <div style={{ flex: 1 }}>
-              <Typography
-                sx={{
-                  fontSize: {
-                    xs: "24px", // Define font size for extra-small screens
-                    sm: "33px", // Define font size for small screens and larger
-                  },
-                  textAlign: "center",
-                  fontWeight: "700",
-                  marginBottom: "24px",
-                }}
-              >
-                Want advice about products?
-              </Typography>
-              <Typography
-                variant="body2"
-                sx={{ marginBottom: "24px", fontSize: "16px" }}
-              >
-                Please leave your information below, an Insurance financial
-                specialist will contact you as soon as possible
-              </Typography>
-              <Stack spacing={{ xs: 1, sm: 2, md: 4 }}>
-                <form onSubmit={formik.handleSubmit}>
-                  <Stack spacing={{ xs: 1, sm: 2, md: 4 }}>
-                    <TextField
-                      id="fullName"
-                      label="customer's full name"
-                      variant="outlined"
-                      size="small"
-                      {...formik.getFieldProps("fullName")}
-                      error={
-                        formik.touched.fullName &&
-                        Boolean(formik.errors.fullName)
-                      }
-                      helperText={
-                        formik.touched.fullName && formik.errors.fullName
-                      }
-                    />
-                    <TextField
-                      id="phoneNumber"
-                      label="Phone number"
-                      variant="outlined"
-                      size="small"
-                      {...formik.getFieldProps("phoneNumber")}
-                      error={
-                        formik.touched.phoneNumber &&
-                        Boolean(formik.errors.phoneNumber)
-                      }
-                      helperText={
-                        formik.touched.phoneNumber && formik.errors.phoneNumber
-                      }
-                    />
-                    <TextField
-                      id="Email"
-                      label="Email"
-                      variant="outlined"
-                      size="small"
-                      {...formik.getFieldProps("Email")}
-                      error={
-                        formik.touched.Email && Boolean(formik.errors.Email)
-                      }
-                      helperText={formik.touched.Email && formik.errors.Email}
-                    />
-                    {/* Other fields */}
-                    <FormControl>
-                      <FormLabel id="demo-row-radio-buttons-group-label">
-                        Gender
-                      </FormLabel>
-                      <RadioGroup
-                        row
-                        aria-labelledby="demo-row-radio-buttons-group-label"
-                        name="row-radio-buttons-group"
-                        {...formik.getFieldProps("gender")}
-                      >
-                        <FormControlLabel
-                          value="female"
-                          control={<Radio />}
-                          label="Female"
-                        />
-                        <FormControlLabel
-                          value="male"
-                          control={<Radio />}
-                          label="Male"
-                        />
-                      </RadioGroup>
-                    </FormControl>
-                    <Autocomplete
-                      disablePortal
-                      options={City}
-                      renderInput={(params) => (
-                        <TextField
-                          {...params}
-                          label="Select city and province"
-                          size="small"
-                        />
-                      )}
-                      value={formik.values.selectedCity}
-                      onChange={(event, value) => {
-                        formik.setFieldValue("selectedCity", value);
-                      }}
-                    />
-                    <Button
-                      variant="contained"
-                      className="btn-hea-info-green"
-                      type="submit"
-                    >
-                      Send information
-                    </Button>
-                  </Stack>
-                </form>
-              </Stack>
-            </div>
-          </Stack>
-          <Stack sx={{ flex: 1 }}>
-            <img
-              src={require("../../Img/muonduoctuvan.jpg")}
-              alt="Insurance House"
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-                borderRadius: "15px",
-              }}
-            />
-          </Stack>
-        </Stack>
+        <SupportRegistration />
         {/* Other products */}
         <Box sx={{ backgroundColor: "#fafafa" }}>
           <Stack
@@ -532,7 +399,6 @@ function LifeInsurance() {
                       <CardActionArea>
                         <CardMedia
                           component="img"
-                         
                           image={`https://localhost:7064/InsuranceType/${item.typeId}`}
                           alt="green iguana"
                           height="240"
@@ -566,114 +432,22 @@ function LifeInsurance() {
         </Box>
         <BusinessLocation />
         <Footer />
-        <Dialog
-          open={openDialog}
-          fullWidth
-          onClose={handleClose}
-          // PaperProps={{
-          //   component: "form",
-          //   onSubmit: (event) => {
-          //     event.preventDefault();
-          //     const formData = new FormData(event.currentTarget);
-          //     const formJson = Object.fromEntries(formData.entries());
-          //     const email = formJson.email;
-          //     console.log(email);
-          //     handleClose();
-          //   },
-          // }}
+
+        {/* snackbar */}
+        <Snackbar
+          open={alertCheck}
+          autoHideDuration={6000}
+          onClose={handleCloseAlert}
         >
-          {/* Dialog */}
-          <DialogTitle> Want advice about products?</DialogTitle>
-          <DialogContent>
-            <form onSubmit={formik.handleSubmit}>
-              <Stack spacing={{ xs: 1, sm: 2, md: 4 }}>
-                <TextField
-                  id="fullName"
-                  label="customer's full name"
-                  variant="outlined"
-                  size="small"
-                  {...formik.getFieldProps("fullName")}
-                  error={
-                    formik.touched.fullName && Boolean(formik.errors.fullName)
-                  }
-                  helperText={formik.touched.fullName && formik.errors.fullName}
-                />
-                <TextField
-                  id="phoneNumber"
-                  label="Phone number"
-                  variant="outlined"
-                  size="small"
-                  {...formik.getFieldProps("phoneNumber")}
-                  error={
-                    formik.touched.phoneNumber &&
-                    Boolean(formik.errors.phoneNumber)
-                  }
-                  helperText={
-                    formik.touched.phoneNumber && formik.errors.phoneNumber
-                  }
-                />
-                <TextField
-                  id="Email"
-                  label="Email"
-                  variant="outlined"
-                  size="small"
-                  {...formik.getFieldProps("Email")}
-                  error={formik.touched.Email && Boolean(formik.errors.Email)}
-                  helperText={formik.touched.Email && formik.errors.Email}
-                />
-                {/* Other fields */}
-                <FormControl>
-                  <FormLabel id="demo-row-radio-buttons-group-label">
-                    Gender
-                  </FormLabel>
-                  <RadioGroup
-                    row
-                    aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
-                    {...formik.getFieldProps("gender")}
-                  >
-                    <FormControlLabel
-                      value="female"
-                      control={<Radio />}
-                      label="Female"
-                    />
-                    <FormControlLabel
-                      value="male"
-                      control={<Radio />}
-                      label="Male"
-                    />
-                  </RadioGroup>
-                </FormControl>
-                <Autocomplete
-                  disablePortal
-                  options={City}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Select city and province"
-                      size="small"
-                    />
-                  )}
-                  value={formik.values.selectedCity}
-                  onChange={(event, value) => {
-                    formik.setFieldValue("selectedCity", value);
-                  }}
-                />
-                {/* <Button
-                      variant="contained"
-                      className="btn-hea-info-green"
-                      type="submit"
-                    >
-                      Send information
-                    </Button> */}
-                <DialogActions>
-                  <Button onClick={handleClose}>Cancel</Button>
-                  <Button type="submit">Subscribe</Button>
-                </DialogActions>
-              </Stack>
-            </form>
-          </DialogContent>
-        </Dialog>
+          <Alert
+            onClose={handleCloseAlert}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            Add information success
+          </Alert>
+        </Snackbar>
       </div>
     </>
   );
