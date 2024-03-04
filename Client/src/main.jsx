147b8b0327fee564ee423784d5690d56263a8f28
nh19941995 <nguyenhieuxtx@gmail.com>
2024-03-04 12:32:45 +0700

    aa

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TableStickyHeader from './Components/Table/TableStickyHeader'
 import CalculateForm from './Components/CalculateForm/CalculateForm'
+import Tickets from './Components/Tickets/Tickets'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      {/*   <TableStickyHeader />*/}
-        {/*<TableStickyHeader />*/}
-      {/*  <Tickets />*/}
+     {/*   <TableStickyHeader />*/}
+        <Tickets/>
         {/*<FormDialog />*/}
         {/*<ComponentA/>*/}
         {/*<DialogTest/>*/}
-
-        <CalculateForm/>
+        {/*<CalculateForm/>*/}
   </React.StrictMode>
 )
